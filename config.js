4333207940c227a3a29d53d4f4ea26ab0f142623
Azkiabot <104640274+Azkiabot@users.noreply.github.com>
2022-04-29 22:20:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,12 +17,12 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285162634171','6285748894171']
-global.web = 'https://youtube.com/channel/UC2hMC3YSXSrDDhqOECXZkUg'
-global.bot = 'NahBotz-MD²'
-global.premium = ['6285748894171']
-global.packname = 'NakataArdnt'
-global.author = 'Nakata'
+global.owner = ['6285846268962','6285846268962']
+global.web = ''
+global.bot = 'AzkiaSBotz'
+global.premium = ['6285846268962']
+global.packname = 'Azkiastore'
+global.author = 'Azkiastore'
 global.sessionName = 'nahh'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
